ac955c5aee6cc1e282c9550b80baa936fadd9412
rudy0628 <s3352250zz@gmail.com>
2022-11-15 15:35:05 +0800

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,25 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="zh-TW">
-				<Head />
+				<Head>
+					<Script
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+						strategy="afterInteractive"
+					/>
+					<Script id="google-analytics" strategy="afterInteractive">
+						{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+					</Script>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
