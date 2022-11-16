9b6ddda353c0c7ae8443917a44ca4b1a3c277629
rudy0628 <s3352250zz@gmail.com>
2022-11-16 14:39:47 +0800

    Add No Result

--- components/Post/PostComments.tsx
+++ components/Post/PostComments.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef, useCallback } from 'react';
 
 import { getPostComments } from '../../services';
 import { PostCommentForm, PostComment } from './';
+import { NoResult } from '../utils';
 import { IComment } from '../../type';
 import cx from 'classnames';
 
@@ -54,40 +55,44 @@ const PostComments = ({ slug }: IProps) => {
 						留言區（{comments.length}）
 					</h1>
 					<hr className="my-4" />
-					<div
-						className={cx(
-							'relative',
-							!displayAllComments &&
-								comments.length > 2 &&
-								'h-[16rem] overflow-y-hidden'
-						)}
-					>
-						{/* comments */}
-						{comments.map((comment: IComment, index: number) => (
-							<PostComment
-								key={comment.id}
-								comment={comment}
-								floor={index + 1}
-								slug={slug}
-								setComments={setComments}
-							/>
-						))}
-						{/* show more comment button */}
-						{comments.length > 2 && (
-							<button
-								className={
-									displayAllComments
-										? 'w-full p-2 h-10 text-sm text-[#90caf9]'
-										: 'absolute bottom-0 left-0 w-full p-2 bg-gradient-to-t from-zinc-50 h-10 text-sm text-[#90caf9] dark:from-zinc-900'
-								}
-								onClick={() => setDisplayAllComments(prevState => !prevState)}
-							>
-								{displayAllComments
-									? '隱藏所有留言'
-									: `展開所有留言（${comments.length}）`}
-							</button>
-						)}
-					</div>
+					{comments && comments.length > 0 && (
+						<div
+							className={cx(
+								'relative',
+								!displayAllComments &&
+									comments.length > 2 &&
+									'h-[16rem] overflow-y-hidden'
+							)}
+						>
+							{/* comments */}
+							{comments.map((comment: IComment, index: number) => (
+								<PostComment
+									key={comment.id}
+									comment={comment}
+									floor={index + 1}
+									slug={slug}
+									setComments={setComments}
+								/>
+							))}
+							{/* show more comment button */}
+							{comments.length > 2 && (
+								<button
+									className={
+										displayAllComments
+											? 'w-full p-2 h-10 text-sm text-[#90caf9]'
+											: 'absolute bottom-0 left-0 w-full p-2 bg-gradient-to-t from-zinc-50 h-10 text-sm text-[#90caf9] dark:from-zinc-900'
+									}
+									onClick={() => setDisplayAllComments(prevState => !prevState)}
+								>
+									{displayAllComments
+										? '隱藏所有留言'
+										: `展開所有留言（${comments.length}）`}
+								</button>
+							)}
+						</div>
+					)}
+					{!comments ||
+						(comments.length === 0 && <NoResult text="暫時沒有任何留言！" />)}
 				</div>
 			</div>
 		</div>
--- components/Post/PostRelatedPosts.tsx
+++ components/Post/PostRelatedPosts.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { getRelatedPosts } from '../../services';
 import { IRelatedPost } from '../../type';
 import moment from 'moment';
+import { NoResult } from '../utils';
 
 interface IProps {
 	categories: {
@@ -28,28 +29,32 @@ const PostRelatedPosts = ({ categories, slug }: IProps) => {
 				<p className="mb-16 font-extrabold text-center md:text-left">
 					閱讀更多
 				</p>
-				{relatedPosts.map((post: IRelatedPost, index: number) => (
-					<Link key={index} href={`/posts/${post.slug}`}>
-						<div className="mb-12 flex flex-col md:flex-row cursor-pointer group">
-							<img
-								src={post.image.url}
-								alt={post.title}
-								className="w-full md:w-48 h-48 object-cover mr-8 rounded-lg group-hover:scale-105 transition-all duration-500"
-							/>
-							<div className="flex-1 mt-4 md:mt-0">
-								<p className="font-extrabold text-xl group-hover:translate-x-2 transition-all duration-700">
-									{post.title}
-								</p>
-								<p className="opacity-70 my-4 group-hover:translate-x-4 transition-all duration-1000 text-[#90caf9] font-extrabold">
-									{moment(post.createdAt).format('YYYY-MM-DD')}
-								</p>
-								<p className="lg:opacity-40 group-hover:opacity-100 transition-all">
-									{post.excerpt}
-								</p>
+				{relatedPosts &&
+					relatedPosts.length > 0 &&
+					relatedPosts.map((post: IRelatedPost, index: number) => (
+						<Link key={index} href={`/posts/${post.slug}`}>
+							<div className="mb-12 flex flex-col md:flex-row cursor-pointer group">
+								<img
+									src={post.image.url}
+									alt={post.title}
+									className="w-full md:w-48 h-48 object-cover mr-8 rounded-lg group-hover:scale-105 transition-all duration-500"
+								/>
+								<div className="flex-1 mt-4 md:mt-0">
+									<p className="font-extrabold text-xl group-hover:translate-x-2 transition-all duration-700">
+										{post.title}
+									</p>
+									<p className="opacity-70 my-4 group-hover:translate-x-4 transition-all duration-1000 text-[#90caf9] font-extrabold">
+										{moment(post.createdAt).format('YYYY-MM-DD')}
+									</p>
+									<p className="lg:opacity-40 group-hover:opacity-100 transition-all">
+										{post.excerpt}
+									</p>
+								</div>
 							</div>
-						</div>
-					</Link>
-				))}
+						</Link>
+					))}
+				{!relatedPosts ||
+					(relatedPosts.length === 0 && <NoResult text="暫時沒有相關文章！" />)}
 			</div>
 		</div>
 	);
