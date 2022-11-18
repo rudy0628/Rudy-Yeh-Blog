4fbc77d0b7aa9e8079976111c5019659cb447f5d
rudy0628 <s3352250zz@gmail.com>
2022-11-18 10:07:06 +0800

    Fixed comments render issue

--- components/Post/PostComments.tsx
+++ components/Post/PostComments.tsx
@@ -30,7 +30,6 @@ const PostComments = ({ slug }: IProps) => {
 
 	useEffect(() => {
 		getPostComments(slug).then(comments => {
-			console.log(comments);
 			setComments(comments);
 			setFloor(comments.length + 1);
 		});
